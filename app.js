b88ec9cf673912a4cd9f875b73e90341b471a324
Liz <lizkristinaphillips@gmail.com>
2017-06-16 11:24:57 -0400

    removes old console.logs and comments from refernce projects

--- app.js
+++ app.js
@@ -23,22 +23,6 @@ app.use(morgan('dev'))
 // placeholder route to test the server
 app.get('/', (req, res, next) => res.send('You\'ve reached Passepartout. I\'m not here right now. Leave your message at the beep!'))
 
-// var start = function(callback) {
-//   request.get({
-//     url: 'aaa.com'
-//   }, function (error, response, body) {
-//     var startingPlace = JSON.parse(body).id;
-//     recurse(startingPlace, otherFunc, function (results) {
-//         console.log ("Recursion finished with results " + results);
-//         callback();
-//     });
-//   });
-// };
-
-// https://www.bennadel.com/blog/3201-exploring-recursive-promises-in-javascript.htm
-// https://mostafa-samir.github.io/async-recursive-patterns-pt2/
-// http://stackoverflow.com/questions/26515671/asynchronous-calls-and-recursion-with-node-js
-// Messing something up in here. Answers are above^^^
 app.get('/crawl', (req, res, next) => {
   console.log('request recieived')
 
@@ -48,8 +32,6 @@ app.get('/crawl', (req, res, next) => {
 
   explore(url)
 
-  // trying this workflow that was found here combined with the technique below
-  // http://stackoverflow.com/questions/26515671/asynchronous-calls-and-recursion-with-node-js
   function explore (url) {
     journal.push(url) // log location in journal
 
@@ -72,24 +54,19 @@ app.get('/crawl', (req, res, next) => {
 
 
 function requestPromise (url, previousJump) {
-  // The structure of our request call
-  // The first parameter is our URL
-  // The callback function takes 3 parameters, an error, response status code and the html
   return new Promise(function (resolve, reject) {
     request(url, (err, response, html) => {
-
-
-      // First we'll check to make sure no errors occurred when making the request
       if (err){
         return reject(err)
       }
 
-      // // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+      // Utilize the cheerio library on the returned html which will essentially give us jQuery functionality
       var $ = cheerio.load(html);
 
-      // // Finally, we'll define the variables we're going to capture
-      const selection = $('a') //all the a tags
+      // grab all the A tags
+      const selection = $('a')
 
+      //get a random url
       const destinationUrl = randomSelection(selection, previousJump)
 
       if (destinationUrl.code === 1) {
