1bc903eda89e927d559dda1a438ff293e8effd44
Liz <lizkristinaphillips@gmail.com>
2017-07-12 17:25:36 -0400

    makes spacing, if/else statements, and comments consistent in randomSelection.js.

--- utilities/randomSelection.js
+++ utilities/randomSelection.js
@@ -1,21 +1,18 @@
 // for testing
 // console.log(selectionValid('wwwreddit.com'))
 
+// sites that should not be jumped to
 const blacklist = ['facebook', 'twitter', 'linkedin', 'apple']
-
 
 function randomSelection (selection) {
   console.log('selecting link')
   for (let attempt = 1; attempt < selection.length; attempt++) {
     console.log(`    attempt number ${attempt}`)
     const possibleSelection = parseSelection(selection)
-    // console.log('possibleSelection in randomSelection', possibleSelection)
-    // console.log('type of possible selection', typeof possibleSelection)
     if (selectionValid(possibleSelection)) {
       console.log('        valid selection')
       return possibleSelection
-    }
-    console.log('        invalid selection')
+    } else console.log('        invalid selection')
   }
   console.log('    maximum attempts reached')
   const error = new Error('maximum attempts for vaid link attempted')
@@ -29,26 +26,17 @@ function parseSelection (selection) {
   const randomNum = Math.random()
   const randomIndex = Math.round(randomNum * (selectionLength - 1))
   const newSelection = selection[randomIndex]
-
   return newSelection ? newSelection.attribs.href : null
 }
 
 function selectionValid (url) {
   if (!url) return false
-  // check for black list sites
+  // check for blacklisted sites
   const burned = blacklist.filter((blackSite) => url.includes(blackSite))
   if (burned.length) return false
   // checks to see if the string starts with www., http://, or https://
-  if (url.match(/(^(http:\/\/|https:\/\/|www\.))/gm)) {
-    return true
-  }
-
-  return false
+  if (url.match(/(^(http:\/\/|https:\/\/|www\.))/gm)) return true
+  else return false
 }
-
-// thought about trying to add the prefix from the last working url
-// function historyPrefixAdd (url, lastUrl) {
-//   lastUrl.split
-// }
 
 module.exports = randomSelection
